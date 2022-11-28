6b0681fb4c5a8e86e68f1b312a11fed84c3a8c90
Aki <arishok.kun1009@gmail.com>
2022-11-28 16:21:45 +1000

    method for array

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿string[] StrArray()
+{
+    int size = 0;
+
+    do
+    {
+        Console.Write("Input size number: ");
+        size = Convert.ToInt32(Console.ReadLine());
+    }
+    while(size <= 0);
+
+    string[] array = new string[size];
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write("Input string: ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
